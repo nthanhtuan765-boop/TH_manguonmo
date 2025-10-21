0893359f28c7c60c9307c810264fbdbb722340a6
nthanhtuan765-boop <nthanhtuan765@gmail.com>
2025-10-21 10:02:09 +0700

    ham main

--- b1.c
+++ b1.c
@@ -1,31 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-    int thang, nam, songay;
+void xemthang(){
+    int thang;
+    printf("nhap thang: ");
+    scanf("%d",&thang);
 
-    printf("Nhap thang (1-12): ");
-    scanf("%d", &thang);
-
-    // Nếu tháng 2 thì cần nhập thêm năm để kiểm tra năm nhuận
-    if (thang == 2) {
-        printf("Nhap nam: ");
-        scanf("%d", &nam);
-
-        if ((nam % 400 == 0) || (nam % 4 == 0 && nam % 100 != 0))
-            songay = 29; // năm nhuận
-        else
-            songay = 28;
-    } 
-    else if (thang == 4 || thang == 6 || thang == 9 || thang == 11)
-        songay = 30;
-    else if (thang == 1 || thang == 3 || thang == 5 || thang == 7 || 
-             thang == 8 || thang == 10 || thang == 12)
-        songay = 31;
-    else {
-        printf("Thang khong hop le!\n");
-        return 1;
+    if(thang == 2){
+        printf("thang co 28 hoac 29 ngay");
+    }
+    if(thang==1 || thang==3 || thang==5 || thang==7 || thang==8 || thang==10 || thang==12){
+        printf("thang co 31 ngay");
     }
 
-    printf("Thang %d co %d ngay.\n", thang, songay);
-    return 0;
+    if(thang==4 || thang==6 || thang==9 || thang==11){
+        printf("thang co 30 ngay");
+    }
+    
 }
