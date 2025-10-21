fa162ef54cf49afef6946e6ae11a81309412ddb8
Vi Khang <vikhang060904@gmail.com>
2025-10-21 10:33:20 +0700

    khang

---
+++ b5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.hh>
+#include<stdbool.h>
+
+void bai5() {
+    int kwh; 
+    printf("Nhap so dien (kWh): "); scanf("%d",&kwh);
+    printf("Tien dien = %lld VND\n", tien_dien(kwh));
+}
+int main()
+{
+    int ch;
+}
